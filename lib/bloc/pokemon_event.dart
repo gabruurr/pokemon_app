dd774506b7e88f089593f1fbfee8185a2bb62530
unknown <gabryelaraujo2019@gmail.com>
2025-06-12 00:08:11 -0300

    feat: implementada lógica inicial  de eventos do BloC

---
+++ lib/bloc/pokemon_event.dart
@@ -0,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+import '../models/pokemon.dart';
+
+abstract class PokemonEvent extends Equatable {
+  const PokemonEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class LoadPokemons extends PokemonEvent {}
+
+class AddPokemon extends PokemonEvent {
+  final Pokemon pokemon;
+
+  const AddPokemon(this.pokemon);
+
+  @override
+  List<Object?> get props => [pokemon];
+}
+
+class UpdatePokemon extends PokemonEvent {
+  final Pokemon pokemon;
+
+  const UpdatePokemon(this.pokemon);
+
+  @override
+  List<Object?> get props => [pokemon];
+}
