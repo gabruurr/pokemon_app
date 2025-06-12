c146f84abda33cda284c0db349f1e30b6444f02e
unknown <gabryelaraujo2019@gmail.com>
2025-06-12 00:05:22 -0300

    feat: implementado mapeamento para entdiade

--- lib/models/pokemon.dart
+++ lib/models/pokemon.dart
@@ -12,4 +12,40 @@ class Pokemon {
     required this.imageAsset,
     required this.estaNaEquipe,
   });
+
+  Pokemon copyWith({
+    int? id,
+    String? nome,
+    String? tipo,
+    String? imageAsset,
+    bool? estaNaEquipe,
+  }) {
+    return Pokemon(
+      id: id ?? this.id,
+      nome: nome ?? this.nome,
+      tipo: tipo ?? this.tipo,
+      imageAsset: imageAsset ?? this.imageAsset,
+      estaNaEquipe: estaNaEquipe ?? this.estaNaEquipe,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'nome': nome,
+      'tipo': tipo,
+      'imageAsset': imageAsset,
+      'estaNaEquipe': estaNaEquipe ? 1 : 0,
+    };
+  }
+
+  factory Pokemon.fromMap(Map<String, dynamic> map) {
+    return Pokemon(
+      id: map['id'],
+      nome: map['nome'],
+      tipo: map['tipo'],
+      imageAsset: map['imageAsset'] ?? 'assets/images/placeholder.png',
+      estaNaEquipe: map['estaNaEquipe'] == 1,
+    );
+  }
 }
