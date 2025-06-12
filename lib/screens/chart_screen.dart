f0f657f6a4a9ce1e4ba3305c432c152b5007b033
unknown <gabryelaraujo2019@gmail.com>
2025-06-12 01:33:46 -0300

    feat: criando metodo com ListView para listar as movimentacoes, com detalhes

--- lib/screens/chart_screen.dart
+++ lib/screens/chart_screen.dart
@@ -25,6 +25,82 @@ class _ChartScreenState extends State<ChartScreen> {
   }
 
 
+
+Widget _buildTimelineView(List<Movement> movements) {
+    if (movements.isEmpty) {
+      return const Center(
+        child: Text('Nenhuma movimentação registrada.',
+            style: TextStyle(fontSize: 18)),
+      );
+    }
+    return ListView.builder(
+      padding: EdgeInsets.all(6),
+      itemCount: movements.length,
+      itemBuilder: (context, index) {
+        final movement = movements[index];
+        final bool paraEquipe = movement.movedTo == 'Equipe';
+        final theme = Theme.of(context);
+        final formattedDate = DateFormat('dd/MM/yyyy \'às\' HH:mm', 'pt_BR')
+            .format(movement.timestamp);
+
+        return Card(
+          margin: const EdgeInsets.only(bottom: 12),
+          elevation: 2,
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              spacing: 8,
+              children: [
+                Image.asset(movement.pokemonImageAsset, width: 50, height: 50),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text.rich(
+                        TextSpan(
+                          text: movement.pokemonName,
+                          style: theme.textTheme.titleMedium
+                              ?.copyWith(fontWeight: FontWeight.bold),
+                          children: [
+                            TextSpan(
+                                text: ' foi movido para a ',
+                                style: theme.textTheme.titleMedium),
+                            TextSpan(
+                              text: movement.movedTo,
+                              style: theme.textTheme.titleMedium?.copyWith(
+                                fontWeight: FontWeight.bold,
+                                color: paraEquipe
+                                    ? Colors.green.shade700
+                                    : Colors.blueGrey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Text(
+                        formattedDate,
+                        style: theme.textTheme.bodySmall
+                            ?.copyWith(color: Colors.grey.shade600),
+                      ),
+                    ],
+                  ),
+                ),
+                Icon(
+                  paraEquipe
+                      ? Icons.arrow_upward_rounded
+                      : Icons.arrow_downward_rounded,
+                  color: paraEquipe ? Colors.green.shade700 : Colors.blueGrey,
+                  size: 30,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+
 Widget _buildPieChartView(List<Pokemon> pokemons) {
     final totalEquipe = pokemons.where((p) => p.estaNaEquipe).length;
     final totalPC = pokemons.where((p) => !p.estaNaEquipe).length;
