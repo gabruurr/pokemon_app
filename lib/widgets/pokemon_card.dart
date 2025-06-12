6112e4dbca723e846da0995525227e9061fb96c5
unknown <gabryelaraujo2019@gmail.com>
2025-06-12 01:10:24 -0300

    feat: implementando card dos pokemom

---
+++ lib/widgets/pokemon_card.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/pokemon_bloc.dart';
+import '../core/utils/type_utills.dart';
+import '../models/pokemon.dart';
+
+class PokemonCard extends StatelessWidget {
+  final Pokemon pokemon;
+
+  const PokemonCard({super.key, required this.pokemon});
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = context.read<PokemonBloc>();
+
+    final typeSymbol = TypeUtils.getSymbolForType(pokemon.tipo);
+    final typeColor = TypeUtils.getColorForType(pokemon.tipo);
+
+    return Card();
+  }
+}
